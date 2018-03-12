39d1e7bf373cdd92f9d6afd197c6357946ddd23e
alexandracch <alexa_theprinces@hotmail.com>
2018-03-12 13:49:03 -0500

    cambios

--- js/app-principal.js
+++ js/app-principal.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     
+
+//     $('.wanderbus').hover(function(){
+//         var appendWanderbus = '<div class="hover">'+
+//         '<a class="col s12 m12 waves-effect waves-light btn" href="https://alexandracch.github.io/social-network-/index.html" target="_blank">Demo</a>'+
+//          '<a class="button-2 col s12 m12 waves-effect waves-light btn" href="https://github.com/alexandracch/social-network" target="_blank">Repositorio</a>'+
+//    '</div>';
+//         $('.botones').append(appendWanderbus);
+//         $('.hover').css({opacity:0.65});
+        
+//     })
+   
 });
